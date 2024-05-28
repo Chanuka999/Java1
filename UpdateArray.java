97790df51312969954a8c7c16bdbd65969e6f00e
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-05-28 00:52:57 -0700

    update the elements in the array

--- UpdateArray.java
+++ UpdateArray.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
 public class UpdateArray {
-    
+    public static void main(String[] args) {
+        
+        int arr[] =new int[10];
+
+        int size =5;
+     Scanner sun = new Scanner(System.in);
+
+     System.out.println("Enter the data:");
+     for(int i=0;i<size;i++){
+        arr[i]=sun.nextInt();
+     }
+     System.out.println("Out of data:");
+     for(int j=0;j<size;j++){
+        System.out.println(arr[j]);
+     }
+     System.out.println("Enter the index:");
+     int update = sun.nextInt();
+
+     System.out.println("Enter the new data:");
+     int data= sun.nextInt();
+
+     arr[update]= data;
+
+     System.out.println("Out of data:");
+     for(int j=0;j<size;j++){
+        System.out.println(arr[j]);
+     }
+    }
 }
