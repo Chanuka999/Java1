611a1fde24d96ce51e2784a9edd2e26b196359c2
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-05-27 21:13:09 -0700

    update arrayImplementatin

--- Array.java
+++ Array.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 public class Array{
     public static void main(String[] args){
-        int[] arr = {10,20,30,40,50};
+        int[] arr =new int[5];
 
+        int size = 5;
+
+        Scanner sun = new Scanner(System.in);
+       
+        for(int x=0;x<size;x++){
+            arr[x] = sun.nextInt();
+  
+        }
           for(int i=0;i<arr.length;i++){
             System.out.println(arr[i]);
           }
