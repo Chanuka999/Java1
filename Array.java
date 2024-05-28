0416f7a1bc4fe6623407c28a318e7b3074243ed2
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-05-27 21:01:37 -0700

    update java file

--- Array.java
+++ Array.java
@@ -2,6 +2,9 @@ public class Array{
     public static void main(String[] args){
         int[] arr = {10,20,30,40,50};
 
-        System.out.println(arr[4]);
+          for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+          }
+      
     }
 }
