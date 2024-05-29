c33fedd915ec2f0850517807dce2e8a6f4063190
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-05-29 12:54:04 -0700

    sort the array in insertion sort

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,3 +1,35 @@
 public class InsertionSort{
-    
+     public static void InsertionsortArray(int[] arr, int size){
+         for(int j=1;j<size;j++){
+            int value=arr[j];
+            int b=j-1;
+
+            while(b>=0 && arr[b]>value){
+                arr[b+1] = arr[b];
+                b=b-1;
+            }
+            arr[b+1] = value;
+         }
+         
+     }
+
+    public static void main(String[] args){
+        int[] arr = {8,5,4,10,2};
+
+        int size = 5;
+
+        System.out.println("UnSorted Array:");
+        for(int i=0;i<size;i++){
+            System.out.println(arr[i]);
+        }
+
+        System.out.println();
+
+        InsertionsortArray(arr, 5);
+
+        System.out.println("Sorted Array:");
+        for(int i=0;i<size;i++){
+            System.out.println(arr[i]);
+    }
+    }
 }
