e326da4d8e2e18bf4f5c694804fa3c501f5c41c3
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-05-30 18:56:30 -0700

    apply push

--- Stack.java
+++ Stack.java
@@ -8,4 +8,20 @@ public class Stack{
        top = -1;
        capacity = size;
     }
+    
+   boolean isFull(){
+       return top+1 == capacity;
+    }
+
+    void push(int x){
+        top++;
+        arr[top] = x;
+        System.out.println("insertion : " + x);
+    }
+    public static void main(String[] args) {
+        Stack myStack = new Stack(5);
+
+        myStack.push(10);
+    }
+  
 }
